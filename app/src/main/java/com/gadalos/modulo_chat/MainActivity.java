11ad541e2ef32c19b9e45423c648f5b2fa493a1a
Gadalos <83663953+gadalos@users.noreply.github.com>
2023-10-31 23:08:27 -0500

    Modulo chat funcional - parte 2

--- app/src/main/java/com/gadalos/modulo_chat/MainActivity.java
+++ app/src/main/java/com/gadalos/modulo_chat/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gadalos.modulo_chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,8 +12,21 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -21,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton btnEnviar;
     private List<Mensaje> mensajeList;
     private MensajeAdapter mensajeAdapter;
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 String pregunta = escribirMensaje.getText().toString();
                 mensajeChat(pregunta, Mensaje.MENSAJE_ENVIADO_YO);
                 escribirMensaje.setText("");
+                llamarAPI(pregunta);
             }
         });
     }
@@ -59,5 +76,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    void respondeGPT(String responde){
+        mensajeChat(responde, Mensaje.MENSAJE_ENVIADO_GPT);
+    }
 
+    void llamarAPI(String pregunta) {
+        //okhttp
+
+        JSONObject jsonBody = new JSONObject();
+
+        try {
+            jsonBody.put("model", "gpt-3.5-turbo");
+            jsonBody.put("prompt", pregunta);
+            jsonBody.put("max_tokens", 150);
+            jsonBody.put("temperature", 0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")
+                .header("Authorization", "Bearer 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")
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                respondeGPT("Error al conectar con el servidor " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    String respuesta = response.body().string();
+                        JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(respuesta);
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String resultado = jsonArray.getJSONObject(0).getString("text");
+                        respondeGPT(resultado.trim());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+            }else {
+                    String errorResponse = response.body().string(); // Obtener el contenido del error como cadena
+                    respondeGPT("Error al conectar con el servidor " + errorResponse);
+                }
+            }
+        });
+    }
 }
